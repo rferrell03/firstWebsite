bf702fd3d8f3e5d4073de64c7cb35853347c44bc
rferrell03 <referrelliii@gmail.com>
2022-12-13 16:00:37 -0600

    Changed the structure of holding pictures, added a cat button

---
+++ script.js
@@ -0,0 +1,30 @@
+function colorButton(){
+    var randomColor = "";
+    randomColor = getRandomColor();
+    document.getElementsByClassName("column")
+}
+
+function getRandomColor() 
+{
+    var color = "";
+    for(var i = 0; i < 3; i++) {
+        var sub = Math.floor(Math.random() * 256).toString(16);
+        color += (sub.length == 1 ? "0" + sub : sub);
+    }
+    return "#" + color;
+}
+
+function changeCat(){
+    const catPics = ["catPics/cat1.jpg","catPics/cat2.jpg","catPics/cat3.jpg","catPics/cat4.jpg"];
+    var i = getRandomNumber(catPics.length);
+    document.getElementById("catPictures").src = catPics[i];
+    console.log(i);
+    
+}
+
+function getRandomNumber(upperBound){
+    var randomNum;
+    randomNum = Math.floor(Math.random() * upperBound);
+    return randomNum;
+}
+function doNothing(){}
